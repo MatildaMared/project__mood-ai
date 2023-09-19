00477e5a13b1aa62800b30c93a407319684c5d08
Matilda Mared <matildamared@live.se>
2023-09-19 20:46:39 +0200

    work on auth

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,9 +1,7 @@
-import { SignUp } from "@clerk/nextjs";
-
 export default function Page() {
   return (
     <div className="h-full flex align-center justify-center">
-      <SignUp afterSignInUrl="/new-user" redirectUrl="/new-user" />
+      <div>Hi</div>
     </div>
   );
 }
