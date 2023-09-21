ee888eb2053ddc6614f5b15a9bac19df609736ae
Matilda Mared <matildamared@live.se>
2023-09-21 07:29:20 +0200

    add auto save when editing journal entry

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,12 +1,27 @@
 "use client";
+import { updateEntry } from "@/utils/api";
 import { JournalEntry } from "@prisma/client";
 import React, { useState } from "react";
+import { useAutosave } from "react-autosave";
 
 function Editor({ entry }: { entry: JournalEntry }) {
   const [value, setValue] = useState(entry.content);
+  const [loading, setLoading] = useState(false);
+
+  useAutosave({
+    data: value,
+    onSave: async (newValue: string) => {
+      setLoading(true);
+      console.log("Will update");
+      const updated = await updateEntry(entry.id, newValue);
+      console.log("Updated", updated);
+      setLoading(false);
+    },
+  });
 
   return (
     <div className="w-full h-full">
+      {loading && <div>...loading</div>}
       <textarea
         value={value}
         onChange={(e) => setValue(e.target.value)}
