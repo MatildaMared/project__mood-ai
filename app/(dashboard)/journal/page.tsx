bd91efd674bf2b7a2eec7f555e424323106b3a56
Matilda Mared <matildamared@live.se>
2023-09-23 13:48:17 +0200

    work on ai functions

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,6 @@
 import EntryCard from "@/components/EntryCard";
 import NewEntryCard from "@/components/NewEntryCard";
+import { analyze } from "@/utils/ai";
 import { getUserByClerkId } from "@/utils/auth";
 import { prisma } from "@/utils/db";
 import Link from "next/link";
@@ -15,6 +16,12 @@ const getEntries = async () => {
       createdAt: "desc",
     },
   });
+
+  analyze(`I'm going to give you a journal entry. I want you to analyze for a few things. I need the mood, a summary, what the subject is, a background color representing the mood, and a text color that gives a contrast ratio of at least 7.00:0 when used on top of the background color. You need to respond back with a formatted JSON like so: {"mood": "", "subject": "", "summary": "", "backgroundColor": "", "textColor": "", "negative": ""}
+  
+  entry:
+  Today was a really great day. I opened a few packs of pokemon cards and had a glass of red wine. We also watched the movie Geostorm.
+  `);
   return entries;
 };
 
