98895ea1bc120774120c56cc04a73f4479b24f1d
Matilda Mared <matildamared@live.se>
2023-09-25 07:11:40 +0200

    add qa function

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,6 +1,10 @@
+import { loadQARefineChain } from "langchain/chains";
+import { Document } from "langchain/document";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { OpenAI } from "langchain/llms/openai";
 import { StructuredOutputParser } from "langchain/output_parsers";
 import { PromptTemplate } from "langchain/prompts";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { Prompt } from "next/font/google";
 import z from "zod";
 
@@ -61,4 +65,28 @@ export const analyze = async (content: string) => {
   } catch (err) {
     console.log(err);
   }
+};
+
+const qa = async (question, entries) => {
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: {
+        id: entry.id,
+        createdAt: entry.createdAt,
+      },
+    });
+  });
+
+  const model = new OpenAI({ temperature: 0, modelName: "gpt-3.5-turbo" });
+  const chain = loadQARefineChain(model);
+  const embeddings = new OpenAIEmbeddings();
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
+  const relevantDocs = await store.similaritySearch(question);
+  const response = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  });
+
+  return response.output_text;
 };
