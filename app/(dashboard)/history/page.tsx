91b82e5d56355400a7bf3348a2dd10b21387ca1c
Matilda Mared <matildamared@live.se>
2023-09-27 07:06:16 +0200

    work on calculating sentinent score

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -6,13 +6,28 @@ const getData = async () => {
   const user = await getUserByClerkId();
   const analyses = await prisma.analysis.findMany({
     where: {
-      
-    }
-  })
-}
+      userId: user.id,
+    },
+    select: {
+      sentimentScore: true,
+    },
+  });
 
-function HistoryPage() {
-  return <div>HistoryPage</div>;
+  const sentimentScoreSum = analyses.reduce(
+    (acc, curr) => acc + curr.sentimentScore,
+    0
+  );
+
+  const averageSentimentScore = Math.round(sentimentScoreSum / analyses.length);
+
+  return { analyses, averageSentimentScore };
+};
+
+async function HistoryPage() {
+  const { analyses, averageSentimentScore } = await getData();
+  console.log(analyses);
+
+  return <div>{averageSentimentScore}</div>;
 }
 
 export default HistoryPage;
