85d2524a6c12f4300e295b7fe13d1461f5f6b421
Matilda Mared <matildamared@live.se>
2023-09-28 06:25:31 +0200

    add first test

---
+++ tests/home.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { vi } from "vitest";
+import Home from "..//app/page";
+
+vi.mock("@clerk/nextjs", () => {
+  return {
+    auth: () =>
+      new Promise((resolve) =>
+        resolve({
+          userId: "user_2NNEqL2nrIRdJ194ndJqAHwEfxC",
+        })
+      ),
+    ClerkProvider: ({ children }) => <div>{children}</div>,
+    useUser: () => ({
+      isSignedIn: true,
+      user: {
+        id: "user_2NNEqL2nrIRdJ194ndJqAHwEfxC",
+        fullName: "Matilda Mared",
+      },
+    }),
+  };
+});
+
+vi.mock("next/font/google", () => {
+  return {
+    Inter: () => ({ className: "inter " }),
+  };
+});
+
+test("Home", async () => {
+  render(await Home());
+  expect(screen.getByText("The best Journal app.")).toBeTruthy();
+})
